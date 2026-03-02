d3caa2c0bc6de1d153cbb8f02f83ced570f63e20
kousei-Git <shailendra700981@gmail.com>
2026-03-02 18:27:20 +0530

    Redesign color flipper UI with pop art theme

--- 01-Beginner-Projects-Core-Fundamentals/01-ColorFlipper/script.js
+++ 01-Beginner-Projects-Core-Fundamentals/01-ColorFlipper/script.js
@@ -1,17 +1,80 @@
-const btn = document.querySelector('.flip-btn')
-const colorCode = document.getElementById('colorCode')
+const flipBtn = document.querySelector('.flip-btn')
+const colorCode = document.getElementById('color-code')
+const btnSimple = document.querySelector('.btn-simple')
+const btnHex = document.querySelector('.btn-hex')
+const toggleBtn = document.querySelector('toggle-btn')
 
-function randomColor(){
-    const str = '0123456789ABCDEF'
+
+const returnHexCode = () => {
+    const str = "ABCDEF1234567890"
     let hexCode = '#'
-    for(let i=0;i<6;i++){
-        hexCode += str[Math.floor(Math.random()*16)]
+    for (let i=1;i<=6;i++){
+        hexCode += str[Math.floor(16*Math.random())]
     }
-    return hexCode;
+    return hexCode
 }
 
-btn.addEventListener('click',()=>{
-    const hexCode = randomColor()
-    document.body.style.background = hexCode
-    colorCode.textContent = hexCode
+let newColor = '#E63946'
+let mode = 'hex'
+
+flipBtn.addEventListener('click', () => {
+    newColor = returnHexCode()
+    document.documentElement.style.setProperty('--body-bg',newColor)
+    displayColorCode(mode,newColor)
+    saveThings(newColor,mode)
+})
+
+const displayColorCode = (mode,color) => {
+    if (mode === 'simple')
+        colorCode.textContent = ntc.name(color)[1]
+    if (mode === 'hex')
+        colorCode.textContent = color
+}
+
+btnHex.addEventListener('click',() => {
+    colorCode.textContent = newColor
+    mode = 'hex'
+    saveThings(newColor,mode)
+    addTranslation()
+})
+
+btnSimple.addEventListener('click',() => {
+    colorCode.textContent = ntc.name(newColor)[1]
+    mode = 'simple'
+    saveThings(newColor,mode)
+    addTranslation()
+})
+
+const addTranslation = () => {
+    if(mode === 'hex'){
+        btnSimple.classList.remove('active')
+        btnHex.classList.add('active')
+    }else{
+        btnSimple.classList.add('active')
+        btnHex.classList.remove('active')
+    }
+}
+
+const saveThings = (color,mode) => {
+    localStorage.setItem('color',color)
+    localStorage.setItem('colorMode',mode)
+    console.log(color,mode)
+}
+
+const getThings = () => {
+    const localStorageColor = localStorage.getItem('color')
+    const localStorageMode = localStorage.getItem('colorMode')
+    console.log(localStorageColor,localStorageMode)
+    return [localStorageColor,localStorageMode]
+}
+
+window.addEventListener('DOMContentLoaded',() => {
+    const [savedColor,savedMode] =  getThings()
+    if(savedColor,savedMode){
+        document.documentElement.style.setProperty('--body-bg',savedColor)
+        mode = savedMode
+        newColor = savedColor
+        displayColorCode(savedMode,savedColor)
+        addTranslation()
+    }
 })
